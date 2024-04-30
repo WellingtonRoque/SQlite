5b6fdb5d29e015cb1caba5d9bdac2267cd308466
WellingtonRoque <78979041+WellingtonRoque@users.noreply.github.com>
2024-04-30 16:19:48 -0300

    Update main.py

--- Aula/main.py
+++ Aula/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 	banco.conecta()
 	banco.criar_tabelas()
 
-	banco.inserir_cliente('Marcos', '11111111111', 'mcastrosouza@live.com')
-	banco.inserir_cliente('Thomas', '22222222222', 'thomas@gmail.com')
+	banco.inserir_cliente('wel', '11111111111', 'wel@gmail.com')
+	banco.inserir_cliente('roque', '22222222222', 'roque@gmail.com')
 
 	banco.desconecta()
